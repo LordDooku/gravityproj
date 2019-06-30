032ec0052ad5057325256f6ca01b5676d6069c02
LordDooku <51995843+LordDooku@users.noreply.github.com>
2019-06-30 18:43:20 +0200

    Add files via upload

--- GravityProject2.cpp
+++ GravityProject2.cpp
@@ -1,4 +1,4 @@
-//============================================================================
+ //============================================================================
 // Name        : Gravity.cpp
 // Author      : 
 // Version     :
@@ -19,9 +19,8 @@
 #include <iostream>
 #include <cmath>
 #include <random>
-#include <stdio.h>      /* printf, scanf, puts, NULL */
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 
 double fRand(double fMin, double fMax) // Methode zum Generieren von zuf�lligen double numbers
@@ -34,12 +33,12 @@ double fRand(double fMin, double fMax) // Methode zum Generieren von zuf�llige
 class mass // Die Klasse der Massenobjekte
 {
 public:
-	float vx;		// Geschwindigkeit in x-Richtung
-	float vy;		// Geschwindigkeit in y-Richtung
-	float r; 		// Radius
-	float beginx; 	// x-Koordinate der Anfangsposition
-	float beginy;	// y-Koordinate der Anfangsposition
-	float m;		// Masse
+	double vx;		// Geschwindigkeit in x-Richtung
+	double vy;		// Geschwindigkeit in y-Richtung
+
+	double beginx; 	// x-Koordinate der Anfangsposition
+	double beginy;	// y-Koordinate der Anfangsposition
+	double m;		// Masse
 
 
 
@@ -59,88 +58,107 @@ int main() {
 
 	int n;  // n ist die Anzahl der Massenobjekte
 	int t;  // t ist die Anzahl der Zeiteinheiten
-	float posx [n];// neue Positionen
-	float posy [n];
-	float Upposx [n][t];
-	float Upposy [n][t];
+	double posx [n];// neue Positionen
+	double posy [n];
+	double Upposx [n][t];
+	double Upposy [n][t];
 	cout << "Bitte gebe die gewuenschte Anzahl der Massenobjekte ein." << endl;
 	cin >> n;
-	cout << "Bitte gebe nun die gew�nschte Anzahl von Zeiteinheiten ein." << endl;
+	cout << "Bitte gebe nun die gewuenschte Anzahl von Zeiteinheiten ein." << endl;
 	cin >> t;
 
 // creates masses
 	mass masses[n]; //Array mit n Massen
-	float sunmass = 10;
-	float newsunmass = 10 * sunmass;
-	float supernewsunmass = 1000000 * newsunmass;
-	float AbsAbs [n];
+	double sunmass = 10;
+	double newsunmass = 10 * sunmass;
+	double supernewsunmass = 1000000 * newsunmass;
+	double AbsAbs [n];
 				for(int i=1; i< n+1; i++){
-					masses [i].vx = i;
-					masses [i].vy = i;
-					masses [i].beginx = i;
-					masses [i].beginy = i;
+					masses [i].vx = i+1;
+					masses [i].vy = i+1+2 * i;
+					masses [i].beginx = i+1;
+					masses [i].beginy = i+2;
 					masses [i].m = 10000;
 				}
 
 // ab hier Abstand
-				float xAbs[n-1]; // Array mit n-1 Abstanden in x-Richtung
-				float yAbs[n-1];
-				float UpxAbs [n][n]; // Array mit den Arrays xAbs (jeder Eintrag ist ein Array)
-				float UpyAbs [n][n];
+			for (int p=1; p<t+1; p++){
 
-				float AbsAbsfinal [n][n]; // Array aus Arrays mit den Betr�gen der Abst�nde
-					for(int j = 0  ;j < n; j++){
+				double xAbs[n]; // Array mit n-1 Abstanden in x-Richtung
+				double yAbs[n];
+				double UpxAbs [n][n]; // Array mit den Arrays xAbs (jeder Eintrag ist ein Array)
+				double UpyAbs [n][n];
+
+				double AbsAbsfinal [n][n]; // Array aus Arrays mit den Betr�gen der Abst�nde
+					for(int k = 1  ;k < n+1; k++){
 
 
-						for(int k=j; k< n-1; k++){
+						for(int i=1; i< n+1; i++){
 
-							xAbs [k] = masses[k].beginx - masses[j].beginx;
-							yAbs [k] = masses[k].beginy - masses[j].beginy;
-							float x = xAbs [k];
-							float y = yAbs [k];
-							AbsAbs [k] = sqrt(x * x + y * y);
-							UpxAbs [j][k] = xAbs[j];
-							UpyAbs [j][k] = yAbs[j];
-							AbsAbsfinal [j][k] = AbsAbs[j];
+							xAbs [i] = masses[i].beginx - masses[k].beginx;
+							yAbs [i] = masses[i].beginy - masses[k].beginy;
+							double x = xAbs [i];
+							double y = yAbs [i];
+							AbsAbs [i] = sqrt(x * x + y * y);
+							UpxAbs [i][k] = xAbs[i];
+							UpyAbs [i][k] = yAbs[i];
+							AbsAbsfinal [i][k] = AbsAbs[i];
 						}
 
 
 					}
 
 // ab hier Kraft
-					float G = 0.00000000006674408; //Gravitationskonstante
-					float Fx[n]; // Kraft in x-Richtung
-					float Fy[n]; // Kraft in y-Richtung
-					float UpFx[n][n];
-					float UpFy[n][n];
+					double G = 0.00000000006674408; //Gravitationskonstante
+					double Fx[n]; // Kraft in x-Richtung
+					double Fy[n]; // Kraft in y-Richtung
+					double UpFx[n][n];
+					double UpFy[n][n];
+
 					UpFx[n][n] = 0;
-						for(int i = 0; i<n; i++){
-							for(int j=i; j<n; j++){
-								Fx[j] = G * (masses[j+1].m / (AbsAbsfinal [i][j+1] * AbsAbsfinal [i][j+1])) * UpxAbs[i][j+1];
-								Fy[j] = G * (masses[j+1].m / (AbsAbsfinal [i][j+1] * AbsAbsfinal [i][j+1])) * UpyAbs[i][j+1];
-								UpFx[i][j] = Fx[i];
-								UpFy[i][j] = Fy[i];
+						for(int k = 1; k<n+1; k++){
+							for(int i=1; i<n+1; i++){
+								double GesFx[n]; // GEsamtkraft in x-Richtung
+								double GesFy[n];
+								GesFx[0] = 0;
+								GesFy[0] = 0;
+									for(int j=1; j < n+1; j++){ //Berechnet Gesamtkraft auf k-tes Teilchen in x-Richtung
+										GesFx[j] = GesFx[j-1]  + (G * masses[j].m / (AbsAbsfinal[j][k] * AbsAbsfinal[j][k] * AbsAbsfinal[j][k]) * UpxAbs[k][i]);
+									}
+								Fx[i] = GesFx[n];
+									for(int j=1; j < n+1; j++){ //Berechnet Gesamtkraft auf k-tes Teilchen in x-Richtung
+										GesFy[j] = GesFy[j-1]  + (G * masses[j].m / (AbsAbsfinal[j][k] * AbsAbsfinal[j][k] * AbsAbsfinal[j][k]) * UpyAbs[k][i]);
+										}
+								Fy[i] = GesFy[n];
+								UpFx[i][k] = Fx[i];
+								UpFy[i][k] = Fy[i];
 							}
 
 						}
 
-//ab hier  Rest
-			for (int k=0; k<t; k++){
 
 
-			     for (int i=0; i<n; i++) {
-				            posx [i]= masses [i].beginx +(masses [i].vx+(UpFx[i][i+1])*k)*k;
-				            posy [i]= masses [i].beginy +(masses [i].vy+(UpFy[i][i+1])*k)*k;
-				            Upposx[i][k] = posx [k];
-				            Upposy[i][k] = posy [k];
-				            cout << Upposx[i][k]<< endl;
+
+
+//ab hier new position
+
+
+
+			     for (int i=1; i<n+1; i++) {
+				            posx [i]= masses [i].beginx +(masses [i].vx+(Fx[i])*p)*p;
+				            posy [i]= masses [i].beginy +(masses [i].vy+(Fy[i])*p)*p;
+				            Upposx[i][p] = posx [i];
+				            Upposy[i][p] = posy [i];
+				            cout << Upposx[i][p]<< endl;
+				            cout << Upposy[i][p]<< endl;
 				            masses[i].beginx = posx[i];
 				            masses[i].beginy = posy[i];
+				            masses[i].vx = masses [i].vx+(Fx[i])*p;
+				            masses[i].vy = masses [i].vx+(Fy[i])*p;
+			     }
 
-			   }
 
-
-						}
+			}
 
 
 
