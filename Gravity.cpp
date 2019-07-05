1fadd3525b140e2dfc9deed79b76b00ae85d10d5
LordDooku <51995843+LordDooku@users.noreply.github.com>
2019-07-05 18:45:41 +0200

    Gravity
    Added proper comments

--- Gravity.cpp
+++ Gravity.cpp
@@ -13,7 +13,7 @@ using namespace std;
 #include <cstdlib>
 
 
-
+//Methode um "random" double numbers zu erstellen
 double rand( double low, double high ) {
     return ( ( double )rand() * ( high - low ) ) / ( double )RAND_MAX + low;
 }
@@ -24,10 +24,10 @@ class mass // Die Klasse der Massenobjekte
 public:
 	double vx;		// Geschwindigkeit in x-Richtung
 	double vy;		// Geschwindigkeit in y-Richtung
-	double vz;
+	double vz;		// Geschwindigkeit in z-Richtung
 	double beginx; 	// x-Koordinate der Anfangsposition
 	double beginy;	// y-Koordinate der Anfangsposition
-	double beginz;
+	double beginz;	// z-Koordinate der Anfangsposition
 	double m;		// Masse
 
 
@@ -39,47 +39,51 @@ int main() {
 double G = 6.067e-11;
 int t;  // Die Anzahl der Zeiteinheiten
 int n ; // Die Anzahl der Massenbjekte
-int c;
+
+int c; // c, d, p, w sind die Variablen die benutzt werde um Specific Time text Dateien zu erstellen
 int d;
 int p;
 int w;
+
 
 cout << "Bitte gebe die gewuenschte Anzahl der Massenobjekte ein." << endl;
 cin >> n;
 cout << "Bitte gebe nun die gewuenschte Anzahl von Zeiteinheiten ein." << endl;
 cin >> t;
-cout << "Bitte gebe nun die 1. gewuenschte Zeit ein, die geplottet werden soll." << endl;
+
+
+cout << "Bitte gebe nun die 1. Specific Time ein." << endl;
 cin >> c;
-cout << "Bitte gebe nun die 2. gewuenschte Zeit ein, die geplottet werden soll." << endl;
+cout << "Bitte gebe nun die 2. Specific Time ein." << endl;
 cin >> d;
-cout << "Bitte gebe nun die 3. gewuenschte Zeit ein, die geplottet werden soll." << endl;
+cout << "Bitte gebe nun die 3. Specific Time ein." << endl;
 cin >> p;
-cout << "Bitte gebe nun die 4. gewuenschte Zeit ein, die geplottet werden soll." << endl;
+cout << "Bitte gebe nun die 4. Specific Time ein." << endl;
 cin >> w;
 
 
-double xAbs[n+1]; // Array mit n-1 Abstanden in x-Richtung
-double yAbs[n+1];
-double zAbs[n+1];
+double xAbs[n+1]; // Array mit n+1 Abstanden in x-Richtung, der 0te Eintrag wird nicht verwendet
+double yAbs[n+1]; // Array mit n+1 Abstanden in y-Richtung, der 0te Eintrag wird nicht verwendet
+double zAbs[n+1]; // Array mit n+1 Abstanden in z-Richtung, der 0te Eintrag wird nicht verwendet
 
-double UpxAbs [n+1][n+1]; // Array mit den Arrays xAbs (jeder Eintrag ist ein Array)
-double UpyAbs [n+1][n+1];
-double UpzAbs [n+1][n+1];
+double UpxAbs [n+1][n+1]; // Hier sind alle Kombinationen der x-Abstaende gespeichert
+double UpyAbs [n+1][n+1]; // Hier sind alle Kombinationen der y-Abstaende gespeichert
+double UpzAbs [n+1][n+1]; // Hier sind alle Kombinationen der z-Abstaende gespeichert
 
 
-double Fx[n+1]; // Kraft in x-Richtung
-double Fy[n+1]; // Kraft in y-Richtung
-double Fz[n+1];
-double UpFx[n+1][n+1];
-double UpFy[n+1][n+1];
-double UpFz[n+1][n+1];
-double GesFx[n+1]; // Gesamtkraft in x-Richtung (Betrag)
-double GesFy[n+1];
-double GesFz[n];
+double Fx[n+1]; // Kraft in x-Richtung fuer je ein Teilchen
+double Fy[n+1]; // Kraft in y-Richtung fuer je ein Teilchen
+double Fz[n+1]; // Kraft in z-Richtung fuer je ein Teilchen
+double UpFx[n+1][n+1]; // Kraft in x-Richtung fuer alle Teilchen
+double UpFy[n+1][n+1]; // Kraft in y-Richtung fuer alle Teilchen
+double UpFz[n+1][n+1]; // Kraft in z-Richtung fuer alle Teilchen
+double GesFx[n+1]; // Gesamtkraft in x-Richtung
+double GesFy[n+1]; // Gesamtkraft in y-Richtung
+double GesFz[n+1]; // Gesamtkraft in z-Richtung
 
-double posx [n+1];// neue Positionen
-double posy [n+1];
-double posz [n+1];
+double posx [n+1]; // neue x-Position nach einem Zeitschritt
+double posy [n+1]; // neue y-Position nach einem Zeitschritt
+double posz [n+1]; // neue z-Position nach einem Zeitschritt
 
 double nvx[n+1]; // nur Hilfsarrays
 double nvy[n+1];
@@ -89,7 +93,7 @@ double u, r, l; // nur Hilfsvariablen
 
 
 
-// creates masses
+// create masses and assign "random" values to speed, initial position and mass
 	mass masses[n+1]; //Array mit n Massen
 
 				for(int i=1; i< n+1; i++){
@@ -102,6 +106,7 @@ double u, r, l; // nur Hilfsvariablen
 					masses[i].m =  rand(1e10, 1e12);
 				}
 
+//Die .txt Dateien, die am Ende ausgegeben werde, werden erstellt
 FILE* fs;
 fs = fopen("GravityProject.txt","w");
 
@@ -164,7 +169,7 @@ for(int j = 1; j<n+1; j++){
 }
 
 
-//Absolute Kraft
+//Gesamtkraft
 for(int j = 1; j<n+1; j++){
 	GesFx[j] = 0;
 	GesFy[j] = 0;
@@ -174,21 +179,22 @@ for(int j = 1; j<n+1; j++){
 	l = 0;
 	for(int i = 1; i<n+1; i++){
 		if(i != j){
-		GesFx[j] = u + UpFx[i][j]; //Falls was falschlaeuft hier i und j tauschen (und fuer y-Komponente)
-		u = GesFx[j];
-		GesFy[j] = r + UpFy[i][j];
-		r=GesFy[j];
-		GesFz[j] = l + UpFz[i][j];
-	    l=GesFz[j];
+				GesFx[j] = u + UpFx[i][j];
+				u = GesFx[j];
+				GesFy[j] = r + UpFy[i][j];
+				r=GesFy[j];
+				GesFz[j] = l + UpFz[i][j];
+				l=GesFz[j];
+		}
 	}
 }
-}
 
 
-cout << "Nach " << j +1   << " Sekunden" << endl;
+cout << "Nach " << j +1   << " Sekunden" << endl; // Macht Ausgabe lesbarer
+
 //new positions
 	for(int i = 1; i<n+1; i++){
-		posx[i] = masses[i].beginx + (masses[i].vx + (GesFx[i] / masses[i].m));
+		posx[i] = masses[i].beginx + (masses[i].vx + (GesFx[i] / masses[i].m)); // Zeitabstand ist Eins
 		posy[i] = masses[i].beginy + (masses[i].vy + (GesFy[i] / masses[i].m));
 		posz[i] = masses[i].beginz + (masses[i].vz + (GesFz[i] / masses[i].m));
 		cout << "(" << posx[i] << "," << posy[i] << "," << posz[i] << ")" << endl;
@@ -202,9 +208,10 @@ cout << "Nach " << j +1   << " Sekunden" << endl;
 		masses[i].vx = nvx[i] + (GesFx[i] / masses[i].m);
 		masses[i].vy = nvy[i] + (GesFy[i] / masses[i].m);
 		masses[i].vz = nvz[i] + (GesFz[i] / masses[i].m);
-		if(j == c){
-	fprintf(ds,"%e %e %e\n", posx[i], posy[i], posz[i]);
-}
+
+		if(j == c){ 		// Specific Time .txt Dateien werden erstellt
+			fprintf(ds,"%e %e %e\n", posx[i], posy[i], posz[i]);
+		}
 
 		if(j == d){
 			fprintf(dp,"%e %e %e\n", posx[i], posy[i], posz[i]);
@@ -220,6 +227,7 @@ cout << "Nach " << j +1   << " Sekunden" << endl;
 	}
 
 }
+
 fclose(fs);
 
 
